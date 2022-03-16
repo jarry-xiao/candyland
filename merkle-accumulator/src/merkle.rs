4da22255ed437fe497f01b349433949026ff2de2
Noah Gundotra <noahgundotra@noahs-mbp.mynetworksettings.com>
2022-03-16 12:58:51 -0400

    add more comments

--- merkle-accumulator/src/merkle.rs
+++ merkle-accumulator/src/merkle.rs
@@ -5,32 +5,42 @@ use std::iter::FromIterator;
 use std::rc::Rc;
 
 pub type Node = [u8; 32];
+
+/// Max number of concurrent changes to tree supported before having to regenerate proofs 
 pub const MAX_SIZE: usize = 64;
+
+/// Max depth of the Merkle tree
 pub const MAX_DEPTH: usize = 10;
-pub const PADDING: usize = 32 - MAX_DEPTH;
+
+pub const PADDING: usize = 32 - MAX_DEPTH; 
+
+/// Used for node parity when hashing
 pub const MASK: usize = MAX_SIZE - 1;
 
-pub fn recompute(mut start: Node, proof: &[Node], path: u32) -> Node {
+/// Recomputes root of the Merkle tree from Node & proof
+pub fn recompute(mut leaf: Node, proof: &[Node], path: u32) -> Node {
     for (ix, s) in proof.iter().enumerate() {
         if path >> ix & 1 == 1 {
-            let res = hashv(&[&start, s.as_ref()]);
-            start.copy_from_slice(res.as_ref());
+            let res = hashv(&[&leaf, s.as_ref()]);
+            leaf.copy_from_slice(res.as_ref());
         } else {
-            let res = hashv(&[s.as_ref(), &start]);
-            start.copy_from_slice(res.as_ref());
+            let res = hashv(&[s.as_ref(), &leaf]);
+            leaf.copy_from_slice(res.as_ref());
         }
     }
-    start
+    leaf
 }
 
 // Off-chain implentation to keep track of nodes
 pub struct MerkleTree {
     pub leaf_nodes: Vec<Rc<RefCell<TreeNode>>>,
+    /// Empty leaf nodes indices we can directly write to
     pub free_list: VecDeque<Rc<RefCell<TreeNode>>>,
     pub root: Node,
 }
 
 impl MerkleTree {
+    /// Calculates updated root from the passed leaves
     pub fn new(leaves: Vec<Node>) -> Self {
         let mut leaf_nodes = vec![];
         for (i, node) in leaves.iter().enumerate() {
@@ -45,68 +55,7 @@ impl MerkleTree {
             root,
         }
     }
-}
 
-#[derive(Clone)]
-pub struct TreeNode {
-    node: Node,
-    left: Option<Rc<RefCell<TreeNode>>>,
-    right: Option<Rc<RefCell<TreeNode>>>,
-    parent: Option<Rc<RefCell<TreeNode>>>,
-    level: u32,
-    id: u128,
-}
-
-pub struct ProofNode {
-    node: Node,
-    is_right: bool,
-}
-
-impl TreeNode {
-    pub fn new(
-        node: Node,
-        left: Rc<RefCell<TreeNode>>,
-        right: Rc<RefCell<TreeNode>>,
-        level: u32,
-        id: u128,
-    ) -> Self {
-        Self {
-            node,
-            left: Some(left),
-            right: Some(right),
-            parent: None,
-            level,
-            id,
-        }
-    }
-
-    pub fn new_empty(level: u32, id: u128) -> Self {
-        Self {
-            node: empty_node(level),
-            left: None,
-            right: None,
-            parent: None,
-            level: level,
-            id,
-        }
-    }
-
-    pub fn assign_parent(node: &mut Rc<RefCell<TreeNode>>, parent: Rc<RefCell<TreeNode>>) {
-        node.borrow_mut().parent = Some(parent);
-    }
-}
-
-pub fn empty_node(level: u32) -> Node {
-    let mut data = [0; 32];
-    if level != 0 {
-        let lower_empty = empty_node(level - 1);
-        let hash = hashv(&[&lower_empty, &lower_empty]);
-        data.copy_from_slice(hash.as_ref());
-    }
-    data
-}
-
-impl MerkleTree {
     /// Builds root from stack of leaves
     pub fn build_root(leaves: &Vec<Rc<RefCell<TreeNode>>>) -> Node {
         let mut tree = VecDeque::from_iter(leaves.iter().map(|x| Rc::clone(x)));
@@ -142,6 +91,8 @@ impl MerkleTree {
         root
     }
 
+    /// Traverses TreeNodes upwards to root from a Leaf TreeNode
+    /// hashing along the way
     pub fn get_proof(&self, idx: usize) -> (Vec<Node>, u32) {
         let mut proof_vec = Vec::<ProofNode>::new();
         let mut node = Rc::clone(&self.leaf_nodes[idx]);
@@ -173,8 +124,9 @@ impl MerkleTree {
         (proof, path)
     }
 
-    fn update_path(&mut self, idx: usize) {
-        let mut node = Rc::clone(&self.leaf_nodes[idx]);
+    /// Updates root from an updated leaf node set at index: `idx` 
+    fn update_root_from_leaf(&mut self, leaf_idx: usize) {
+        let mut node = Rc::clone(&self.leaf_nodes[leaf_idx]);
         loop {
             let ref_node = Rc::clone(&node);
             if ref_node.borrow().parent.is_none() {
@@ -207,13 +159,78 @@ impl MerkleTree {
         self.root
     }
 
-    pub fn add_leaf(&mut self, leaf: Node, idx: usize) {
-        self.leaf_nodes[idx].borrow_mut().node = leaf;
-        self.update_path(idx)
+    pub fn add_leaf(&mut self, leaf: Node, leaf_idx: usize) {
+        self.leaf_nodes[leaf_idx].borrow_mut().node = leaf;
+        self.update_root_from_leaf(leaf_idx)
     }
 
-    pub fn remove_leaf(&mut self, idx: usize) {
-        self.leaf_nodes[idx].borrow_mut().node = [0; 32];
-        self.update_path(idx)
+    pub fn remove_leaf(&mut self, leaf_idx: usize) {
+        self.leaf_nodes[leaf_idx].borrow_mut().node = [0; 32];
+        self.update_root_from_leaf(leaf_idx)
     }
 }
+
+#[derive(Clone)]
+pub struct TreeNode {
+    node: Node,
+    left: Option<Rc<RefCell<TreeNode>>>,
+    right: Option<Rc<RefCell<TreeNode>>>,
+    parent: Option<Rc<RefCell<TreeNode>>>,
+    level: u32,
+    /// ID needed to figure out whether we came from left or right child node
+    /// when hashing path upwards
+    id: u128,
+}
+
+impl TreeNode {
+    pub fn new(
+        node: Node,
+        left: Rc<RefCell<TreeNode>>,
+        right: Rc<RefCell<TreeNode>>,
+        level: u32,
+        id: u128,
+    ) -> Self {
+        Self {
+            node,
+            left: Some(left),
+            right: Some(right),
+            parent: None,
+            level,
+            id,
+        }
+    }
+
+    pub fn new_empty(level: u32, id: u128) -> Self {
+        Self {
+            node: empty_node(level),
+            left: None,
+            right: None,
+            parent: None,
+            level: level,
+            id 
+        }
+    }
+
+    /// Allows to propagate parent assignment
+    pub fn assign_parent(node: &mut Rc<RefCell<TreeNode>>, parent: Rc<RefCell<TreeNode>>) {
+        node.borrow_mut().parent = Some(parent);
+    }
+}
+
+pub struct ProofNode {
+    node: Node,
+    is_right: bool,
+}
+
+/// Calculates hash of empty nodes up to level i
+/// TODO: cache this
+pub fn empty_node(level: u32) -> Node {
+    let mut data = [0; 32];
+    if level != 0 {
+        let lower_empty = empty_node(level - 1);
+        let hash = hashv(&[&lower_empty, &lower_empty]);
+        data.copy_from_slice(hash.as_ref());
+    }
+    data
+}
+
