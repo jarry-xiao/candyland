b540529231897234de1cee60b33dae4ac7571e5b
steveluscher <me+github@steveluscher.com>
2022-04-01 10:12:24 -0500

    Prevent folks from resizing the UI on mobile

--- gummyroll-crud-ui/pages/_app.tsx
+++ gummyroll-crud-ui/pages/_app.tsx
@@ -1,13 +1,22 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import SearchBar from "../components/SearchBar";
 
 import * as styles from "../styles/app.css"; // Side-effectful import that adds global styles.
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={styles.shell}>
-      <SearchBar />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, user-scalable=no"
+        />
+      </Head>
+      <div className={styles.shell}>
+        <SearchBar />
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
