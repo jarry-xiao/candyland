f5dea681b6adad2e26e103db1a2ba624bc6b11fc
steveluscher <me+github@steveluscher.com>
2022-04-01 10:00:55 -0500

    Install and configure style framework and UI library

--- gummyroll-crud-ui/next.config.js
+++ gummyroll-crud-ui/next.config.js
@@ -1,6 +1,9 @@
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const withVanillaExtract = createVanillaExtractPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withVanillaExtract(nextConfig);
