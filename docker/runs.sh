102338018caf23a48df845a34856bf74ee3d5191
jarry-xiao <jarry.xiao@gmail.com>
2022-06-16 15:11:38 -0500

    Update bubblegum key

--- docker/runs.sh
+++ docker/runs.sh
@@ -24,7 +24,7 @@ cat << EOL > accountsdb-plugin-config.json
             "GRoLLMza82AiYN7W9S9KCCtCyyPRAQP2ifBy4v4D5RMD",
             "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb",
             "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL",
-            "BGUMzZr2wWfD2yzrXFEWTK2HbdYhqQCP2EZoPEkZBD6o"
+            "BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY"
         ]
     },
     "transaction_selector" : {
@@ -34,7 +34,7 @@ cat << EOL > accountsdb-plugin-config.json
             "GRoLLMza82AiYN7W9S9KCCtCyyPRAQP2ifBy4v4D5RMD",
             "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb",
             "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL",
-            "BGUMzZr2wWfD2yzrXFEWTK2HbdYhqQCP2EZoPEkZBD6o"
+            "BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY"
         ]
     }
 }
@@ -52,7 +52,7 @@ args=(
   --reset
   --rpc-port 8899
   --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s /so/mpl_token_metadata.so
-  --bpf-program BGUMzZr2wWfD2yzrXFEWTK2HbdYhqQCP2EZoPEkZBD6o /so/bubblegum.so
+  --bpf-program BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY /so/bubblegum.so
   --bpf-program Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS /so/gummyroll_crud.so
   --bpf-program GRoLLMza82AiYN7W9S9KCCtCyyPRAQP2ifBy4v4D5RMD /so/gummyroll.so
   --bpf-program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA /so/spl_token.so
