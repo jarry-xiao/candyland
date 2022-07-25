b6455c9b362b91b91093c3d9ee77d24444707df0
Sarah Strange <strange1120@gmail.com>
2022-07-25 12:14:16 -0400

    feat(digital_asset_types): assets by owner ready for rpc testing

--- digital_asset_types/src/dapi/assets_by_owner.rs
+++ digital_asset_types/src/dapi/assets_by_owner.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 use crate::dao::prelude::{Asset, AssetData};
 use crate::dao::{asset, asset_authority, asset_creators, asset_data, asset_grouping, cl_items};
 use crate::dapi::asset::{get_content, to_authority, to_creators, to_grouping};
@@ -33,89 +35,74 @@ pub async fn get_assets_by_owner(
 
     let assets = paginator.fetch_page((page - 1).try_into().unwrap()).await?;
 
-    let built_assets = join_all(assets.into_iter().map(|(asset, data)| async move {
-        let interface = match asset.specification_version {
-            1 => Interface::NftOneZero,
-            _ => Interface::Nft,
-        };
+    let filter_assets: Result<Vec<_>, _> = assets
+        .into_iter()
+        .map(|(asset, asset_data)| match asset_data {
+            Some(asset_data) => Ok((asset, asset_data)),
+            _ => Err(DbErr::RecordNotFound("Asset Not Found".to_string())),
+        })
+        .collect();
 
-        let content = if let Some(data) = data {
-            let res = get_content(&asset, &data).unwrap();
-            Some(res)
-        } else {
-            None
-        };
+    let build_asset_list = filter_assets?
+        .into_iter()
+        .map(|(asset, asset_data)| async move {
+            let interface = match asset.specification_version {
+                1 => Interface::NftOneZero,
+                _ => Interface::Nft,
+            };
 
-        let authorities = asset_authority::Entity::find()
-            .filter(asset_authority::Column::AssetId.eq(asset.id.clone()))
-            .all(db);
+            let content = get_content(&asset, &asset_data).unwrap();
 
-        let creators = asset_creators::Entity::find()
-            .filter(asset_creators::Column::AssetId.eq(asset.id.clone()))
-            .all(db);
+            let authorities = asset_authority::Entity::find()
+                .filter(asset_authority::Column::AssetId.eq(asset.id.clone()))
+                .all(db)
+                .await
+                .unwrap();
 
-        let grouping = asset_grouping::Entity::find()
-            .filter(asset_grouping::Column::AssetId.eq(asset.id.clone()))
-            .all(db);
+            let creators = asset_creators::Entity::find()
+                .filter(asset_creators::Column::AssetId.eq(asset.id.clone()))
+                .all(db)
+                .await
+                .unwrap();
 
-        //             let rpc_authorities = to_authority(authorities);
-        //             let rpc_creators = to_creators(creators);
-        //             let rpc_groups = to_grouping(grouping);
+            let grouping = asset_grouping::Entity::find()
+                .filter(asset_grouping::Column::AssetId.eq(asset.id.clone()))
+                .all(db)
+                .await
+                .unwrap();
 
-        grouping
-    }))
-    .await;
+            let rpc_authorities = to_authority(authorities);
+            let rpc_creators = to_creators(creators);
+            let rpc_groups = to_grouping(grouping);
 
-    // let handles: Vec<RpcAsset> = join_all(
-    //     asset_list
-    //         .into_iter()
-    //         .map(|(asset, data)| async move {
-    //             let interface = match asset.specification_version {
-    //                 1 => Interface::NftOneZero,
-    //                 _ => Interface::Nft,
-    //             };
+            RpcAsset {
+                interface,
+                id: bs58::encode(asset.id).into_string(),
+                content: Some(content),
+                authorities: Some(rpc_authorities),
+                compression: Some(Compression {
+                    eligible: asset.compressible,
+                    compressed: asset.compressed,
+                }),
+                grouping: Some(rpc_groups),
+                royalty: Some(Royalty {
+                    royalty_model: asset.royalty_target_type.into(),
+                    target: asset.royalty_target.map(|s| bs58::encode(s).into_string()),
+                    percent: (asset.royalty_amount as f64) * 0.0001,
+                    locked: false,
+                }),
+                creators: Some(rpc_creators),
+                ownership: Ownership {
+                    frozen: asset.frozen,
+                    delegated: asset.delegate.is_some(),
+                    delegate: asset.delegate.map(|s| bs58::encode(s).into_string()),
+                    ownership_model: asset.owner_type.into(),
+                    owner: bs58::encode(asset.owner).into_string(),
+                },
+            }
+        });
 
-    //             let content = if let Some(data) = data {
-    //                 let res = get_content(&asset, &data).unwrap();
-    //                 Some(res)
-    //             } else {
-    //                 None
-    //             };
-
-    //             let grouping = asset_grouping(asset, db)?;
-    //             let rpc_authorities = to_authority(authorities);
-    //             let rpc_creators = to_creators(creators);
-    //             let rpc_groups = to_grouping(grouping);
-
-    //             RpcAsset {
-    //                 interface,
-    //                 id: bs58::encode(asset.id).into_string(),
-    //                 content,
-    //                 authorities: Some(rpc_authorities),
-    //                 compression: Some(Compression {
-    //                     eligible: asset.compressible,
-    //                     compressed: asset.compressed,
-    //                 }),
-    //                 grouping: Some(rpc_groups),
-    //                 royalty: Some(Royalty {
-    //                     royalty_model: asset.royalty_target_type.into(),
-    //                     target: asset.royalty_target.map(|s| bs58::encode(s).into_string()),
-    //                     percent: (asset.royalty_amount as f64) * 0.0001,
-    //                     locked: false,
-    //                 }),
-    //                 creators: Some(rpc_creators),
-    //                 ownership: Ownership {
-    //                     frozen: asset.frozen,
-    //                     delegated: asset.delegate.is_some(),
-    //                     delegate: asset.delegate.map(|s| bs58::encode(s).into_string()),
-    //                     ownership_model: asset.owner_type.into(),
-    //                     owner: bs58::encode(asset.owner).into_string(),
-    //                 },
-    //             }
-    //         })
-    //         .collect::<Vec<RpcAsset>>(),
-    // )
-    // .await;
+    let built_assets = futures::future::join_all(build_asset_list).await;
 
     Ok(AssetList {
         total: todo!(),
@@ -123,6 +110,6 @@ pub async fn get_assets_by_owner(
         page: todo!(),
         before: todo!(),
         after: todo!(),
-        items: todo!(),
+        items: built_assets,
     })
 }
